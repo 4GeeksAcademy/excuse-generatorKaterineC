78e3696f7c25bd191850e262a171f5301d2ed3a0
Katerine Céspedes <katerinecesbal@hotmail.com>
2024-09-18 12:39:13 +0000

    Excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,43 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const who = [
+    "El hamster del servidor ",
+    "El becario con miedo a los teclados ",
+    "El programador en modo zombie ",
+    "El pato de goma consultor ",
+    "El algoritmo rebelde de inteligencia artificial "
+  ];
+
+  const action = [
+    "hackeó sin querer ",
+    "invirtió todos los bytes de ",
+    "declaró la guerra a ",
+    "se fue de vacaciones con ",
+    "tradujo a klingon "
+  ];
+
+  const what = [
+    "la página de login ",
+    "todos los comentarios en el código ",
+    "la base de datos de memes ",
+    "el framework experimental ",
+    "los test unitarios fallidos "
+  ];
+
+  const when = [
+    "en plena madrugada ",
+    "justo antes del deploy ",
+    "durante la demo con el CEO ",
+    "en pleno eclipse lunar ",
+    "cuando la cafetera explotó "
+  ];
+
+  let a = Math.floor(Math.random() * who.length);
+  let b = Math.floor(Math.random() * action.length);
+  let c = Math.floor(Math.random() * what.length);
+  let d = Math.floor(Math.random() * when.length);
+  document.getElementById("excuse").innerHTML =
+    who[a] + action[b] + what[c] + when[d];
 };
